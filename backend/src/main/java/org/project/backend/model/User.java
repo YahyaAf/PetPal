bff8d01037f9f03b813c41a04c8b24240258471d
Yahya afadisse <yahyaafadisse92@gmail.com>
2026-02-08 17:16:05 +0100

    remove authentification process from my project

--- backend/src/main/java/org/project/backend/model/User.java
+++ backend/src/main/java/org/project/backend/model/User.java
@@ -1,5 +1,6 @@
 package org.project.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,6 +29,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String motDePasse;
 
